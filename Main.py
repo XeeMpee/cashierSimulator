8e327922b58dab9c17d1b5aee88b28405a9a8311
XeeMpee <xempee@gmail.com>
2018-05-13 12:43:49 +0200

    EventQueuePattern

--- Main.py
+++ Main.py
@@ -1,6 +1,17 @@
 import pygame
 from Models.Object import *
 
+
+def eventsQueue():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                return False
+    return True
+
+
 if __name__ == "__main__":
     print("Hello Darkness, my old friend!")
 
@@ -11,14 +22,15 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode(windowSize)
 
     spriteGroup = pygame.sprite.Group()
-    normalImage = pygame.image.load("./blender.png")
+    normalImage = pygame.image.load("./Files/blender.png")
     sprite1 = Object(normalImage, normalImage, normalImage, (40,40), (60,60))
     spriteGroup.add(sprite1)
     spriteGroup.draw(screen)
 
     con = True
     while con:
-        screen.fill((255,255,255))
+        screen.fill((255, 255, 255))
+        con = eventsQueue()
 
         spriteGroup.draw(screen)
         pygame.display.update()
